766f881e2ed33c2ae9867aff81559571691e1077
Romeu Miranda Borges <romeuborges19@gmail.com>
2023-11-10 16:45:31 -0300

    feat: adicionando back-end

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"calculadora/controller"
+	"calculadora/repository"
+	"calculadora/service"
+	"embed"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/context"
+	"github.com/gorilla/mux"
+)
+
+func main(){
+	db, err := repository.NewDB()
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer db.Close()
+
+	mux := mux.NewRouter()
+
+	dao := repository.NewDAO()
+	operatorService := service.NewOperationService(dao)
+	c := controller.NewController(operatorService, db)
+
+	var static embed.FS
+
+	mux.Handle("/static/", http.FileServer(http.FS(static)))
+
+}
---
+++ repository/db.go
@@ -0,0 +1,42 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+const (
+	host = "localhost"
+	port = "5432"
+	dbname = "db_calculator"
+	user = "postgres"
+	password = "romeulindo"
+)
+
+var DB *sql.DB
+
+type DAO interface{
+	NewOperationQuery() OperationQuery
+}
+
+type dao struct {}
+
+func NewDAO() DAO {
+	return &dao{}
+}
+
+func NewDB() (*sql.DB, error) {
+	dbconn := fmt.Sprintf("host=%s port=%s dbname=%s sslmode=disable user=%s password=%s", host, port, dbname, user, password)
+	DB, err := sql.Open("postgres", dbconn)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	return DB, nil
+}
+
+func (d *dao) NewOperationQuery() OperationQuery {
+	return &operationQuery{}
+}
